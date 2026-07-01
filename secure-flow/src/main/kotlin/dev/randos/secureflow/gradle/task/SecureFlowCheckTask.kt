bac00af0630b1e51e7c3939f2d8b44a95f693a4d
Vishal Kumar <vsnappy1@gmail.com>
2026-07-01 10:10:54 -0500

    Enable build caching for SecureFlowCheckTask

--- secure-flow/src/main/kotlin/dev/randos/secureflow/gradle/task/SecureFlowCheckTask.kt
+++ secure-flow/src/main/kotlin/dev/randos/secureflow/gradle/task/SecureFlowCheckTask.kt
@@ -7,13 +7,18 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 
+@CacheableTask
 abstract class SecureFlowCheckTask : DefaultTask() {
     @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.RELATIVE)
     abstract val scanDirectory: DirectoryProperty
 
     @get:OutputDirectory
